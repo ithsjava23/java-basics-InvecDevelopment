9486b85573185a904d715a89b8015dbb0b94dedb
Oscar Risö <oscar.risoe@gmail.com>
2023-09-17 17:14:30 +0200

    Meny samt inmatning

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,51 @@
 package org.example;
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        Scanner scanner = new Scanner(System.in);
+        char choice;
+        int[] elpriser = new int[24];
+        //Meny+val
+        do {
+            System.out.print("\nElpriser");
+            System.out.print("\n=======");
+            System.out.print("\n1. Inmatning");
+            System.out.print("\n2. Min, Max och Medel");
+            System.out.print("\n3. Sortera");
+            System.out.print("\n4. Bästa Laddningstid (4h)");
+            System.out.print("\ne. Avsluta");
+            System.out.print("\nVälj ett alternativ: ");
+            choice = scanner.next().charAt(0);
+
+            switch (choice) {
+                case '1':
+                    inmatning(scanner, elpriser);
+                    break;
+                case '2':
+                    break;
+                case '3':
+                    break;
+                case '4':
+                    break;
+                case 'e':
+                case 'E':
+                    System.out.print("\nProgrammet Avslutas");
+                    break;
+                default:
+                    System.out.print("\nError");
+            }
+
+        }            while (choice != 'e' && choice != 'E');
+
+        scanner.close();
+    }
+    public static void inmatning(Scanner scanner, int [] elpriser) {
+        System.out.print("\nInmatning av elpriser (i öre per Kwh):\n");
+        for (int i = 0; i < elpriser.length; i++) {
+            System.out.print("Timme " + i + ": ");
+            elpriser[i] = scanner.nextInt();
+        }
     }
 }
